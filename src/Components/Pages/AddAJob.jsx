cd7ae2436595a3eefffed139bca0dbacd7699590
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-05-13 04:43:59 +0600

    All jobs Page Created

--- src/Components/Pages/AddAJob.jsx
+++ src/Components/Pages/AddAJob.jsx
@@ -1,7 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css'
+// import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
 import { AuthContext } from '../AuthProvider/AuthProvider';
-import add from '../../assets/start2.png'
+import add from '../../assets/job3.jpg';
 
 const AddAJob = () => {
     //Dynamic Title
@@ -9,6 +13,8 @@ const AddAJob = () => {
         document.title = "Career Bridge | Add a Job"
     },[])
 
+    const [startDate, setStartDate] = useState(new Date());
+    // const [selectedDate,setSelectedDate] = useState(new Date());
     const {user} = useContext(AuthContext);
     const handleAdd = (event) =>{
         event.preventDefault();
@@ -48,13 +54,13 @@ const AddAJob = () => {
         })
     }
     return (
-            <div className="hero bg-base-200 border rounded-3xl w-5/6 mx-auto">
+            <div className="hero bg-base-200 border rounded-3xl w-5/6 mx-auto ">
                         <div className="card-body w-full rounded-3xl flex flex-row gap-8 shadow-2xl bg-base-100">
                             {/* IMAGE----PORTION */}
-                            <div className="" >
+                            <div className="w-1/3" >
                                 <img className='w-full h-full rounded-3xl' src={add} alt="add a job" />
                             </div>
-                            <div>
+                            <div className='w-2/3'>
                                 <form onSubmit={handleAdd} className="">
                                 {/* Title Section */}
                                 <div data-aos="fade-left" data-aos-duration="2000" className="text-left mt-4">
@@ -63,9 +69,7 @@ const AddAJob = () => {
                                 </div>
                                 {/* ---------FORM---------- */}
                                 <div data-aos="fade-up" data-aos-duration="3000">
-                                        {/* Input Section */}
-                                    <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
-                                        <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
+                                    <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
                                             {/* PHOTO */}
                                             <div className="form-control">
                                             <label className="label">
@@ -74,7 +78,7 @@ const AddAJob = () => {
                                             <input type="text" placeholder="photo URL"
                                             name="photo" className="input input-bordered" required />
                                             </div>
-                                            {/* SPOT_NAME */}
+                                            {/* JOB_TITLE */}
                                             <div className="form-control">
                                             <label className="label">
                                                 <span className="label-text">Job Title</span>
@@ -82,17 +86,7 @@ const AddAJob = () => {
                                             <input type="text" placeholder="Job Title"
                                             name="job" className="input input-bordered" required />
                                             </div>
-                                            {/*SHORT------DESCRIPTION*/}
-                                            <div className="form-control col-span-1 md:col-span-2 ">
-                                                <label className="label">
-                                                    <span className="label-text">Job Description</span>
-                                                </label>
-                                                <textarea className="textarea textarea-bordered h-[236px]" name="description" placeholder="Describe the spot . . ."></textarea>
-                                            </div>
-                                        </div>
-                                        {/* -------------------------------------------------------- */}
-                                        <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
-                                            {/*   COUNTRY-----NAME */}
+                                            {/*   JOB-----CATEGORY */}
                                             <div className="form-control">
                                             <label className="label">
                                                 <span className="label-text">Job Category</span>
@@ -100,16 +94,7 @@ const AddAJob = () => {
                                             <input type="text" placeholder="Job Category"
                                             name="category" className="input input-bordered" required />
                                             </div>
-                                            {/* LOCATION------- */}
-                                            <div className="form-control">
-                                            <label className="label">
-                                                <span className="label-text">Job Posting Date</span>
-                                            </label>
-                                            <input type="text" placeholder="Job Posting Date"
-                                            name="postdate" className="input input-bordered" required />
-                                            </div>
-                                            
-                                            {/* AVERAGE----COST */}
+                                            {/* SALARY-----RANGE */}
                                             <div className="form-control">
                                             <label className="label">
                                                 <span className="label-text">Salary Range</span>
@@ -117,47 +102,72 @@ const AddAJob = () => {
                                             <input type="text" placeholder="Salary Range"
                                             name="range" className="input input-bordered" required />
                                             </div>
-                                            {/* SEASONALITY------ */}
-                                            <div className="form-control">
-                                            <label className="label">
-                                                <span className="label-text">Application Deadline</span>
-                                            </label>
-                                            <input type="text" placeholder="Job Posting Date"
-                                            name="deadline" className="input input-bordered" required />
-                                            </div>
-                                            {/* TRAVEL-----TIME */}
-                                            <div className="form-control">
-                                            <label className="label">
-                                                <span className="label-text">Job Applicant's Number</span>
-                                            </label>
-                                            <input type="text" placeholder="Job Applicant's Number"
-                                            name="number" className="input input-bordered" required />
-                                            </div>
-                                            </div>
                                             {/* USER ----NAME */}
                                             <div className="form-control">
-                                            <label className="label">
-                                                <span className="label-text">User Name</span>
-                                            </label>
-                                            <input type="text" placeholder="User Name"
-                                            name="username" className="input input-bordered" required />
+                                                <label className="label">
+                                                    <span className="label-text">User Name</span>
+                                                </label>
+                                                <input type="text" placeholder="User Name"
+                                                name="username" className="input input-bordered" required />
                                             </div>
                                             {/* USER-----EMAIL */}
                                             <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text">Email</span>
+                                                </label>
+                                                <input type="email" placeholder="email"
+                                                name="email" className="input input-bordered" required />
+                                            </div>
+                                            {/* POSTING DATE----------- */}
+                                            <div className="form-control">
                                             <label className="label">
-                                                <span className="label-text">Email</span>
+                                                <span className="label-text">Job Posting Date</span>
                                             </label>
-                                            <input type="email" placeholder="email"
-                                            name="email" className="input input-bordered" required />
+                                            <input type="text" placeholder="Job Posting Date"
+                                            name="postdate" className="input input-bordered" required />
+                                            </div>
+                                            {/* DEADLINE------ */}
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text">Application Deadline</span>
+                                                </label>
+                                                {/* <Calendar onChange={(value)=>setSelectedDate(value)} ></Calendar> */}
+                                                <DatePicker className='input input-bordered text-gray-400' name='deadline' required selected={startDate} onChange={(date) => setStartDate(date)} formatDate ="dd/MM/yyyy" />
+                                                {/* <input type="text" placeholder="Job Posting Date"
+                                                name="deadline" className="input input-bordered" required /> */}
+                                            </div>
+                                            
+                                            {/* JOB APPLICANT'S NUMBER */}
+                                            
+                                            <div className="form-control">
+                                                <label className="label">
+                                                    <span className="label-text">Job Applicant's Number</span>
+                                                </label>
+                                                <input type="text" placeholder="Job Applicant's Number"
+                                                name="number" className="input input-bordered" required />
+                                            </div>
                                             </div>
                                         </div>
-                                        {/* ------------------------------------ */}
+                                            
+
+
+
+                                        {/* Input Section */}
+                                    <div className='grid grid-cols-1 md:grid-cols-4 gap-3'>
+                                            {/*JOB------DESCRIPTION*/}
+                                            <div className="form-control col-span-1 md:col-span-4 ">
+                                                <label className="label">
+                                                    <span className="label-text">Job Description</span>
+                                                </label>
+                                                <textarea className="textarea textarea-bordered h-[100px]" name="description" placeholder="Describe the spot . . ."></textarea>
+                                            </div>
+                                            
+                                             
                                     </div>
                                     {/* Submit Button */}
                                     <div className="form-control mt-6">
                                         <input type="submit" value="Add" className="btn basic-btn" />
                                     </div>
-                                
                             </form>
                             </div>
                         </div>
--- src/Components/Pages/AllJobs.jsx
+++ src/Components/Pages/AllJobs.jsx
@@ -1,12 +1,82 @@
 import React, { useEffect } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import Lottie from "lottie-react";
+// import SoloTrip from "/src/SoloTrip.json";
 
 const AllJobs = () => {
+    // Dynamic Title
     useEffect(()=>{
         document.title = "Career Bridge | All Jobs"
     },[])
+
+    const jobs = useLoaderData();
     return (
         <div>
-            All Jobs Will be shown here
+            <div  className='w-1/3 mx-auto' >
+                    {/* <Lottie className='w-2/3 mx-auto' animationData={SoloTrip} /> */}
+            </div>
+            <div>
+                <div className='mx-auto w-4/5 text-center'>
+                    <h3 className="text-2xl md:text-3xl mt-12 ">Watch Jobs added by People From Around The World!</h3>
+                    <h2 className="text-base md:text-xl text-gray-500 mt-4 mb-8">There are about {jobs.length} Jobs listed till Now. You can add more! </h2>
+                </div>
+            </div>
+            {/* the LIST OF ALL SPOTS */}
+            <div data-aos="fade-up" data-aos-duration="3000" >
+                <table className="table">
+                        <thead className='mx-0 text-center'>
+                        <tr>
+                            <th className='hidden md:flex'>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Job Title</th>
+                            <th>Job Posting Date</th>
+                            <th className="hidden md:flex flex-row gap-8">
+                                  <h2>Application Deadline</h2>
+                                  <h2>Salary Range</h2>
+                            </th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                jobs.map(job=>  <tr key={job._id}>
+                                    <th  className='hidden md:flex'>
+                                        <label>
+                                            <input type="checkbox" className="checkbox" />
+                                        </label>
+                                    </th>
+                                    <td>
+                                        <div className="flex items-center gap-3">
+                                            <div className="avatar hidden md:flex">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={job.photo} />
+                                            </div>
+                                            </div>
+                                            <div>
+                                            <div className="font-bold"> {job.job} </div>
+                                            <div className="text-sm opacity-50"> {job.location} </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {job.postdate}
+                                        <br/>
+                                    </td>
+                                    <td className="hidden md:flex flex-row gap-16 ">
+                                        <h2>{job.deadline}</h2>
+                                        <h2>{job.range}</h2>
+                                     </td>
+                                    <th>
+                                        <Link ><button className='btn basic-btn' >View Details</button></Link>
+                                    </th>
+                                </tr> )
+                            }
+                        </tbody>
+                    </table>
+            </div>
         </div>
     );
 };
